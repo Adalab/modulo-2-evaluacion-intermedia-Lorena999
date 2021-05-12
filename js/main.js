f1a3af58178b26e8c90b3bcd6c15b64acc384005
lorena <lorena@MacBook-Pro-de-lorena.local>
2021-05-12 10:54:47 +0200

    add changes

--- js/main.js
+++ js/main.js
@@ -1,30 +1,34 @@
 const buttonProof = document.querySelector('.js-button-proof');
 const userNumber = document.querySelector('js-user-number');
 const resultUserNumber = document.querySelector('js-result-user-number');
-
+const addUser = document.querySelector('.js-addUser');
 
 function getRandomNumber(max) {
     const number = Math.ceil(Math.random() * max);
     return number
-    
 }
 const Randomnumber = getRandomNumber(100);
     console.log(Randomnumber);
 
     function compareUserNumber () {
-        if (Randomnumber < userNumber) {
-            resultUserNumber.value = 'Demasiado alto'
-        } else if (Randomnumber < userNumber) {
-            resultUserNumber.value = 'Demasiado bajo'
+        const userNumberValue = userNumber.value;
+        if (Randomnumber < userNumberValue) {
+            resultUserNumber.value = 'Demasiado alto';
+        } else if (Randomnumber < userNumberValue) {
+            resultUserNumber.value = 'Demasiado bajo';
         }
-        else if (Randomnumber === userNumber) {
-            resultUserNumber.value = 'Has ganado campeona'
+        else if (Randomnumber === userNumberValue) {
+            resultUserNumber.value = 'Has ganado campeona';
         }
     
   }
 
+  function addAttemps() {
+const addUserForm = addUser
+  }
+
 function handleClicButton () {
     compareUserNumber();
-    //addAttemps();
+    addAttemps();
   }
   buttonProof.addEventListener('click', handleClicButton);
