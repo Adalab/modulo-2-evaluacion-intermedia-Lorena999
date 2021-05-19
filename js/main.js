e83986a94e15de9186bdb2db9bdada133c1f004c
Lorena <lorena.perez@gmail.com>
2021-05-19 22:06:41 +0200

    change html and js-basic

--- js/main.js
+++ js/main.js
@@ -1,35 +1,33 @@
-const buttonProof = document.querySelector('.js-button-proof');
-const userNumber = document.querySelector('.js-user-number');
-const resultUserNumber = document.querySelector('.js-result-user-number');
-const addUser = document.querySelector('.js-addUser');
+const buttonProof = document.querySelector(".js-button-proof");
+const userNumber = document.querySelector(".js-user-number");
+const resultUserNumber = document.querySelector(".js-result-user-number");
+const addUser = document.querySelector(".js-addUser");
 
 function getRandomNumber(max) {
-    const number = Math.ceil(Math.random() * max);
-    return number
+  const number = Math.ceil(Math.random() * max);
+  return number;
 }
 const Randomnumber = getRandomNumber(100);
-    console.log(Randomnumber);
+console.log(Randomnumber);
 
-    function compareUserNumber () {
-        const userNumberValue = parseInt(userNumber.value);
-        
-        if (Randomnumber < userNumberValue) {
-            resultUserNumber.value = 'Demasiado alto';
-        } else if (Randomnumber > userNumberValue) {
-            resultUserNumber.value = 'Demasiado bajo';
-        }
-        else if (Randomnumber === userNumberValue) {
-            resultUserNumber.value = 'Has ganado campeona';
-        }
-    
-  }
+function compareUserNumber() {
+  const userNumberValue = parseInt(userNumber.value);
 
-  function addAttemps() {
-//const addUserForm = addUser
+  if (Randomnumber < userNumberValue) {
+    resultUserNumber.value = "Demasiado alto";
+  } else if (Randomnumber > userNumberValue) {
+    resultUserNumber.value = "Demasiado bajo";
+  } else {
+    resultUserNumber.value = "Has ganado campeona";
   }
+}
 
-function handleClicButton () {
-    compareUserNumber();
-    //addAttemps();
-  }
-  buttonProof.addEventListener('click', handleClicButton);
+function addAttemps() {
+  //const addUserForm = addUser
+}
+
+function handleClicButton() {
+  compareUserNumber();
+  //addAttemps();
+}
+buttonProof.addEventListener("click", handleClicButton);
